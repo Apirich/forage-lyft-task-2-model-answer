83173d89e371e89f68333d2bd024fd2807e6693f
ApiRich <anhpikall@gmail.com>
2023-08-24 17:36:23 -0700

    Refoldering unittest from test_car.py into test_battery.py and test_engine.py

--- test/test_engine.py
+++ test/test_engine.py
@@ -1,44 +1,9 @@
 import unittest
 from datetime import datetime
 
-from battery.spindler_battery import SpindlerBattery
-from battery.nubbin_battery import NubbinBattery
-
 from engine.capulet_engine import CapuletEngine
 from engine.willoughby_engine import WilloughbyEngine
 from engine.sternman_engine import SternmanEngine
-
-
-class TestSpindler(unittest.TestCase):
-    def test_battery_should_be_serviced(self):
-        today = datetime.today().date()
-        last_service_date = today.replace(year = today.year - 3)
-
-        battery = SpindlerBattery(today, last_service_date)
-        self.assertTrue(battery.needs_service())
-
-    def test_battery_should_not_be_serviced(self):
-        today = datetime.today().date()
-        last_service_date = today.replace(year = today.year - 1)
-
-        battery = SpindlerBattery(today, last_service_date)
-        self.assertFalse(battery.needs_service())
-
-
-class TestNubbin(unittest.TestCase):
-    def test_battery_should_be_serviced(self):
-        today = datetime.today().date()
-        last_service_date = today.replace(year = today.year - 5)
-
-        battery = NubbinBattery(today, last_service_date)
-        self.assertTrue(battery.needs_service())
-
-    def test_battery_should_not_be_serviced(self):
-        today = datetime.today().date()
-        last_service_date = today.replace(year = today.year - 2)
-
-        battery = NubbinBattery(today, last_service_date)
-        self.assertFalse(battery.needs_service())
 
 
 class TestCapulet(unittest.TestCase):
